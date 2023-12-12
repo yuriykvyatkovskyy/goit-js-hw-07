7b4661e6842bbe9f647537ca701cb7293d85c5b1
yuriykvyatkovskyy <yuriykreik20021997@gmail.com>
2023-12-12 13:37:32 +0200

    commit task-03

--- js/task-03.js
+++ js/task-03.js
@@ -1,1 +1,9 @@
 console.log('Task-03')
+
+const nameInput = document.querySelector("#name-input")
+const nameOutput = document.querySelector("#name-output")
+
+nameInput.addEventListener("input", (event) => {
+    const inputValue = event.target.value.trim()
+    nameOutput.textContent = inputValue ? inputValue : "Anonymous"
+})
