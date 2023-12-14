45cffc740be4bd64a053431738a51bf5992585ea
yuriykvyatkovskyy <yuriykreik20021997@gmail.com>
2023-12-14 14:27:57 +0200

    commit task6

--- js/task-06.js
+++ js/task-06.js
@@ -6,10 +6,33 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-const input = document.querySelector('.input');
+const inputNumber = document.querySelector('.input');
 const buttonCreate = document.querySelector('[data-create]');
 const buttonDestroy = document.querySelector('[data-destroy]');
+const newBoxes = document.querySelector('#boxes');
 
-const createBoxes = amount => {
+function createBoxes (amount) {
+  let size = 30;
+  const elements = [];
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    size += 1;
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+
+    elements.push(box);
+  }
   
+  newBoxes.appendChild(...elements);
 }
+
+buttonCreate.addEventListener('click', () => {
+  const amount = inputNumber.value;
+  createBoxes(amount);
+  inputNumber.value = '';
+})
+
+buttonDestroy.addEventListener('click', () => {
+  newBoxes.innerHTML = '';
+})
