6609a19e4e8a7182a447416bb943c878bda1bb4c
yuriykvyatkovskyy <yuriykreik20021997@gmail.com>
2023-12-14 15:59:39 +0200

    commit Task-1

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,13 @@
 console.log('Task-01')
+
+let listItem = document.querySelectorAll('li.item');
+const calcItem = listItem.length;
+console.log(`Number of categories: ${calcItem}`);
+
+listItem.forEach(item => {
+    const nameItem = item.querySelector('h2');
+    console.log(`Category: ${nameItem.textContent}`);
+
+    const calculate = item.querySelectorAll('li').length;
+    console.log(`Elements: ${calculate}`);
+});
